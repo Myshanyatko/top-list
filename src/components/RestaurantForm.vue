7eda4987db1685de970d11e5ae38f150ef0492b2
myshanyatko <arinkakogyt@gmail.com>
2024-02-11 16:32:24 +0700

    restaurant form ui

--- src/components/RestaurantForm.vue
+++ src/components/RestaurantForm.vue
@@ -1,59 +1,193 @@
 <template>
-  <form @submit.prevent>
-    <input type="text" v-model="restaurant.name" />
-    <h3>Прайс</h3>
-    <!-- <input type="number" min="0" max="2.5" v-model="restaurant.price.a" /> -->
-    <!-- <input type="number" min="0" max="2.5" v-model="restaurant.price.p" /> -->
-    <input
-      type="text"
-      v-model="restaurant.assessments.price.a"
-      placeholder="Описание"
-    />
-    <div class="">
-      <button @click="createRestaurant">Сохранить</button>
-    </div>
-  </form>
+  <v-form
+    v-model="form"
+    @submit.prevent="submit"
+  >
+    <v-card>
+      <v-card-title>
+        <span class="text-h5">Добавление заведения</span>
+      </v-card-title>
+      <v-card-text>
+        <v-container>
+          <v-row>
+            <v-col>
+              <v-text-field
+                v-model="restaurant.name"
+                label="Название"
+                type="text"
+                required
+                :rules="requiredRule"
+              >
+              </v-text-field>
+            </v-col>
+          </v-row>
+          <v-row>
+            <v-col cols="6">
+              <v-autocomplete
+                label="Город"
+                :items="cites"
+                v-model="restaurant.city"
+              ></v-autocomplete>
+            </v-col>
+          </v-row>
+          <v-row>
+            <v-col>
+              <h4>Оценка прайса</h4>
+            </v-col>
+          </v-row>
+          <v-row>
+            <v-col cols="6">
+              <v-text-field
+                v-model="restaurant.assessments.price.a"
+                label="Арина"
+                type="number"
+                required
+                :rules="confinesRule1"
+                max="2.5"
+              ></v-text-field>
+            </v-col>
+            <v-col cols="6">
+              <v-text-field
+                v-model="restaurant.assessments.price.p"
+                label="Паша"
+                type="number"
+                :rules="confinesRule1"
+                required
+              ></v-text-field>
+            </v-col>
+          </v-row>
+          <v-row>
+            <v-col>
+              <h4>Оценка интерьера</h4>
+            </v-col>
+          </v-row>
+          <v-row>
+            <v-col cols="6">
+              <v-text-field
+                v-model="restaurant.assessments.interior.a"
+                label="Арина"
+                type="number"
+                required
+                :rules="confinesRule1"
+              ></v-text-field>
+            </v-col>
+            <v-col cols="6">
+              <v-text-field
+                v-model="restaurant.assessments.interior.p"
+                label="Паша"
+                type="number"
+                required
+                :rules="confinesRule1"
+              ></v-text-field>
+            </v-col>
+          </v-row>
+          <v-row>
+            <v-col>
+              <h4>Оценка еды</h4>
+            </v-col>
+          </v-row>
+          <v-row>
+            <v-col cols="6">
+              <v-text-field
+                v-model="restaurant.assessments.taste.a"
+                label="Арина"
+                type="number"
+                required
+                :rules="confinesRule2"
+              ></v-text-field>
+            </v-col>
+            <v-col cols="6">
+              <v-text-field
+                v-model="restaurant.assessments.taste.p"
+                label="Паша"
+                type="number"
+                required
+                :rules="confinesRule2"
+              ></v-text-field>
+            </v-col>
+          </v-row>
+        </v-container>
+      </v-card-text>
+      <v-card-actions>
+        <v-spacer></v-spacer>
+        <v-btn
+          variant="text"
+          @click="this.$emit('close')"
+        >
+          Закрыть
+        </v-btn>
+        <v-btn
+          color="primary"
+          :disabled="!form"
+          variant="text"
+          type="submit"
+        >
+          Сохранить
+        </v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-form>
 </template>
 
 <script>
 export default {
+  name: "RestaurantForm",
   data() {
     return {
+      cites: ["Томск", "Новосибирск", "Красноярск", "Новокузнецк"],
+      requiredRule: [
+        (value) => {
+          if (value) return true;
+          return "Обязательное поле";
+        },
+      ],
+      confinesRule1: [
+        (value) => {
+          if (value) return true;
+          return "Обязательное поле";
+        },
+        (value) => {
+          if (+value > 2.5 || +value < 0)
+            return "Значение должно быть от 0 до 2.5";
+          return true;
+        },
+      ],
+      confinesRule2: [
+        (value) => {
+          if (value) return true;
+          return "Обязательное поле";
+        },
+        (value) => {
+          if (+value > 5 || +value < 0) return "Значение должно быть от 0 до 5";
+          return true;
+        },
+      ],
+      form: false,
       restaurant: {
-        name: "",
+        name: null,
+        city: "Томск",
         assessments: {
-          general: "",
+          general: null,
           price: {
-            a: "",
-            p: "",
+            a: null,
+            p: null,
           },
           taste: {
-            a: "",
-            p: "",
+            a: null,
+            p: null,
           },
           interior: {
-            a: "",
-            p: "",
+            a: null,
+            p: null,
           },
         },
       },
     };
   },
-  // watch: {
-  //   restaurant: function () {
-  //     console.log("sfd");
-  //     this.restaurant.general =
-  //       +this.restaurant?.assessments?.price?.a +
-  //       +this.restaurant?.assessments?.price?.p +
-  //       +this.restaurant?.assessments?.taste?.a +
-  //       +this.restaurant?.assessments?.taste?.p +
-  //       +this.restaurant?.assessments?.interior?.a +
-  //       +this.restaurant?.assessments?.interior?.p;
-  //   },
-  // },
 
   methods: {
-    createRestaurant() {
+    submit() {
+      this.$emit("close");
       this.restaurant.id = Date.now();
       this.restaurant.assessments.general =
         +this.restaurant?.assessments?.price?.a +
@@ -64,25 +198,23 @@ export default {
         +this.restaurant?.assessments?.interior?.p;
       this.$emit("create", this.restaurant);
       this.restaurant = {
-        name: "",
+        name: null,
         assessments: {
-          general: "",
+          general: null,
           price: {
-            a: "",
-            p: "",
+            a: null,
+            p: null,
           },
           taste: {
-            a: "",
-            p: "",
+            a: null,
+            p: null,
           },
           interior: {
-            a: "",
-            p: "",
+            a: null,
+            p: null,
           },
         },
       };
-      // this.restaurant.price.a = 0;
-      // this.restaurant.price.p = 0;
     },
   },
 };
--- src/main.js
+++ src/main.js
@@ -1,4 +1,30 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from "vue";
+import App from "./App.vue";
+import { loadFonts } from "./plugins/webfontloader";
 
-createApp(App).mount('#app')
+// Vuetify
+import "@mdi/font/css/materialdesignicons.css";
+import "vuetify/styles";
+import { createVuetify } from "vuetify";
+import * as components from "vuetify/components";
+import * as directives from "vuetify/directives";
+import colors from 'vuetify/util/colors'
+
+const vuetify = createVuetify({
+  components,
+  directives,
+  theme: {
+    themes: {
+      light: {
+        dark: false,
+        colors: {
+          primary: colors.indigo.darken1, // #E53935
+          secondary: colors.indigo.lighten4, // #FFCDD2
+        },
+      },
+    },
+  },
+});
+loadFonts();
+
+createApp(App).use(vuetify).mount("#app");
